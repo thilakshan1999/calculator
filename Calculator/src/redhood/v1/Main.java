2c54bf29529473c8082d322d620535fe7fa08826
Thilakshan <thilakshanlk@gmail.com>
2021-10-08 08:42:16 +0530

    v2-input validation2.0

--- Calculator/src/redhood/v1/Main.java
+++ Calculator/src/redhood/v1/Main.java
@@ -1,19 +1,20 @@
 package redhood.v1;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args)
-    {
-        int choice,i=0;
-        int data[]=new int[2];
-        int output=0;
+    public static void main(String[] args) {
+        int choice, i = 0;
+        int data[] = new int[10];
+        int output = 0;
         try {
-            File Obj = new File("Calculator/file.txt");
+            File Obj = new File("Calculator/src/redhood/v1/file.txt");
             Scanner myReader = new Scanner(Obj);
             while (myReader.hasNextInt()) {
-                 data[i] = myReader.nextInt();
+                data[i] = myReader.nextInt();
                 i++;
             }
             myReader.close();
@@ -23,19 +24,18 @@ public class Main {
         }
         System.out.println("Welcome to calculator program");
         System.out.print("select what commant want to do\n(1-addition)\n(2-subraction)\n(3-multiplication)\n:");
-        choice=new Scanner (System.in).nextInt();
-        switch(choice)
-        {
+        choice = new Scanner(System.in).nextInt();
+        switch (choice) {
             case 1:
-                output=data[0]+data[1];
+                output = data[0] + data[1];
                 break;
             case 2:
-                output=data[0]-data[1];
+                output = data[0] - data[1];
                 break;
             case 3:
-                output=data[0]*data[1];
+                output = data[0] * data[1];
                 break;
         }
-        System.out.println("Output:"+output);
+        System.out.println("Output:" + output);
     }
 }
--- Calculator/src/redhood/v2/Main.java
+++ Calculator/src/redhood/v2/Main.java
@@ -1,20 +1,36 @@
 package redhood.v2;
 
-public class Main
-{
-    public static void main(String[] args) {
-        if(args.length==0)
-        {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
             System.out.println("Please provide the operation as argument");
             return;
         }
-        String operator=args[0];
-        if(!(operator.equals("add")||operator.equals("sub")||operator.equals("mul")))
-        {
+        String operator = args[0];
+        if (!(operator.equals("add") || operator.equals("sub") || operator.equals("mul"))) {
             System.out.println("Plese provide add ,sub or mul as operational argument");
             return;
         }
-        System.out.println(args[0]);
+        List<String> numberStr = Files.readAllLines(Paths.get("Calculator/src/redhood/v2/Number.txt"));
+        Double number1 = Double.valueOf(numberStr.get(0));
+        Double number2 = Double.valueOf(numberStr.get(1));
+
+
+        double result = 0;
+        if (operator.equals("add"))
+            result = number1 + number2;
+        else if (operator.equals("sub"))
+            result = number1 - number2;
+        else if (operator.equals("mul"))
+            result = number1 * number2;
+
+        System.out.println("the result is" + result);
+
 
     }
 }
