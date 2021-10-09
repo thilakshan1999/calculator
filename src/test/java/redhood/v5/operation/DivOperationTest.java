f9fe4055e4b547cf8b5f06141467e57b4e4d8970
Thilakshan <thilakshanlk@gmail.com>
2021-10-10 00:07:35 +0530

    unit test 2.0

--- src/test/java/redhood/v5/operation/DivOperationTest.java
+++ src/test/java/redhood/v5/operation/DivOperationTest.java
@@ -3,6 +3,7 @@ package redhood.v5.operation;
 import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThrows;
 
 public class DivOperationTest {
     @Test
@@ -16,9 +17,7 @@ public class DivOperationTest {
     @Test
     public void should_not_divide_by_zero() throws InvalidOperatioException {
         DivOperation divOperation=new DivOperation();
-        double result=divOperation.execute(new double[]{6.0,0.0});
-
-        assertThat(result,is(0.0));
+        assertThrows(InvalidOperatioException.class,()->{divOperation.execute(new double[]{6.0,0.0});});
     }
 
 }
---
+++ src/test/java/redhood/v5/operation/OperationFactoryTest.java
@@ -0,0 +1,26 @@
+package redhood.v5.operation;
+
+import org.junit.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+public class OperationFactoryTest {
+
+    @Test
+    public void should_return_add_operation_object_when_operator_is_add()
+    {
+        OperationFactory operationFactory=new OperationFactory();
+        Operation operation=operationFactory.getInstance("add");
+        assertThat(operation,instanceOf(AddOperation.class));
+
+    }
+    @Test
+    public void should_return_sub_operation_object_when_operator_is_sub()
+    {
+        OperationFactory operationFactory=new OperationFactory();
+        Operation operation=operationFactory.getInstance("sub");
+        assertThat(operation,instanceOf(SubOperation.class));
+
+    }
+}
